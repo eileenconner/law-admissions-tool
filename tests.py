10b3b6f8d6c967ffb21e4b04da34ab5821753d5d
Eileen Conner <eileenconner@gmail.com>
2015-11-19 13:12:38 -0800

    provided contrast color for user gpa/lsat on chart.js charts

--- tests.py
+++ tests.py
@@ -1,48 +1,51 @@
 """Python unit tests for law school admission tool"""
 
-import server
 import unittest
-# import doctest
 
-
-# def load_tests(loader, tests, ignore):
-#     """Also run doctests and file-based doctests."""
-
-#     tests.addTests(doctest.DocTestSuite(server))
-#     return tests
+from server import app
+from model import db
+from model import User
 
 
 class SchoolTests(unittest.TestCase):
     """Tests for law school admission tool"""
 
     def setUp(self):
-        pass
+        """Set up database for testing purposes"""
+        self.app = app.test_client()
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+        db.app = app
+        db.init_app(app)
+        db.create_all()
+        print "setup ran"
 
     def tearDown(self):
-        pass
+        """Remove testing db"""
+        db.session.remove()
+        db.drop_all()
+        print "teardown ran"
 
-    def test_testing_working(self):
-        """Make sure this file is actually executing tests by raising a failure."""
-        self.assertTrue(False)
+    # def test_testing_working(self):
+    #     """Make sure this file is actually executing tests by raising a failure."""
+    #     self.assertTrue(False)
+
+    def test_user_creation(self):
+        """Test creating a user in db"""
+        user = User(email='test@test.com', password='password', gpa='3.5', lsat='160')
+        db.session.add(user)
+        db.session.commit()
+        self.assertEqual(user.email, 'test@test.com')
+        db.session.rollback()
 
 
-class IntegrationTests(unittest.TestCase):
-    """Tests Flask/html pieces for law school admission tool"""
+# class IntegrationTests(unittest.TestCase):
+#     """Tests Flask/html pieces for law school admission tool"""
 
-    def setUp(self):
-        # return test web browser w test_client()
-        self.client = server.app.test_client()
-        # include other things -- fake data, session user, fake db to test?
-
-    def tearDown(self):
-        # which one?
-        # server.db.session.rollback()
-        server.db.session.close()
-
-    def test_homepage(self):
-        result = server.test_client.get('/')
-        self.assertEqual(result.status_code, 200)
-        # self.assertIn('<h1>Test</h1>', result.data)
+#     def test_homepage(self):
+#         result = server.test_client.get('/')
+#         self.assertEqual(result.status_code, 200)
+#         # self.assertIn('<h1>Test</h1>', result.data)
 
 
 # run unit tests if running file in shell
