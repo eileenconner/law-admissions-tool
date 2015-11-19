82b425bf542ff50771bea1271b39ab0130215868
Eileen Conner <eileenconner@gmail.com>
2015-11-18 17:09:51 -0800

    tests.py runs

--- tests.py
+++ tests.py
@@ -2,22 +2,32 @@
 
 import server
 import unittest
-import doctest
+# import doctest
 
 
-def load_tests(loader, tests, ignore):
-    """Also run our doctests and file-based doctests."""
+# def load_tests(loader, tests, ignore):
+#     """Also run doctests and file-based doctests."""
 
-    tests.addTests(doctest.DocTestSuite(server))
-    return tests
+#     tests.addTests(doctest.DocTestSuite(server))
+#     return tests
 
 
 class SchoolTests(unittest.TestCase):
     """Tests for law school admission tool"""
 
-    def test_user_stat_result(self):
-        # insert something useful here
-        #self.assertEqual(len(server.find_user_gpa_lsat()), 2)
+    def setUp(self):
+        # return test web browser w test_client()
+        self.client = server.app.test_client()
+
+    def tearDown(self):
+        pass
+        # which one?
+        server.db.session.rollback()
+        server.db.session.close()
+
+    def test_testing_working(self):
+        """Make sure this file is actually executing tests by raising a failure."""
+        self.assertTrue(False)
 
 
 # run unit tests if running file in shell
