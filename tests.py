5632036757e89eed92758ea4b0462e25e1098504
Eileen Conner <eileenconner@gmail.com>
2015-11-18 20:04:05 -0800

    added bootstrap scripts to base.html

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-"""Python unit tests for law school admissions tool"""
+"""Python unit tests for law school admission tool"""
 
 import server
 import unittest
@@ -16,18 +16,33 @@ class SchoolTests(unittest.TestCase):
     """Tests for law school admission tool"""
 
     def setUp(self):
-        # return test web browser w test_client()
-        self.client = server.app.test_client()
+        pass
 
     def tearDown(self):
         pass
-        # which one?
-        server.db.session.rollback()
-        server.db.session.close()
 
     def test_testing_working(self):
         """Make sure this file is actually executing tests by raising a failure."""
         self.assertTrue(False)
+
+
+class IntegrationTests(unittest.TestCase):
+    """Tests Flask/html pieces for law school admission tool"""
+
+    def setUp(self):
+        # return test web browser w test_client()
+        self.client = server.app.test_client()
+        # include other things -- fake data, session user, fake db to test?
+
+    def tearDown(self):
+        # which one?
+        # server.db.session.rollback()
+        server.db.session.close()
+
+    def test_homepage(self):
+        result = server.test_client.get('/')
+        self.assertEqual(result.status_code, 200)
+        # self.assertIn('<h1>Test</h1>', result.data)
 
 
 # run unit tests if running file in shell
